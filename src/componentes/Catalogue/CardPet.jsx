52681d44843e8fe9a529f4a7f93ac58fc6f60423
unknown <sebastianwilches2@gmail.com>
2023-05-16 14:49:31 -0500

    Representación info de BD en 'PetAdoption'

--- src/componentes/Catalogue/CardPet.jsx
+++ src/componentes/Catalogue/CardPet.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import './CardPet.css'
 
-export default function CardPet({info:{attributes, id}}) {
+export default function CardPet({ info: { attributes, id } }) {
   let urlPet = `http://localhost:1337${attributes.picture.data.attributes.url}`;
 
   return (
     <section className='CardPet'>
-        <div className='CardImage'><img src={urlPet} alt={`CardImage${id}`}/></div>
-        <div className='CardInfo'>
-            <h4 className='CardTitle'>{attributes.name}</h4>
-            <h4 className='CardTitle'>{console.log(urlPet)}</h4>
-            <button className='CardBtn'>Conóceme</button>
-        </div>
+      <div className='CardImage'><img src={urlPet} alt={`CardImage${id}`} /></div>
+      <div className='CardInfo'>
+        <h4 className='CardTitle'>{attributes.name}</h4>
+        {/* <button className='CardBtn'>Conóceme</button> */}
+        <Link to={`/mascota/${id}`}>
+          <button className='CardBtn'>Conóceme</button>
+        </Link>
+      </div>
 
 
     </section>
--- src/componentes/Pet/PetAdoption.jsx
+++ src/componentes/Pet/PetAdoption.jsx
@@ -1,22 +1,47 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './PetAdoption.css'
 
 import CardTest from '../../IMGs/CardTest.jpg'
 
 export default function PetAdoption() {
+
+  const { id } = useParams();
+  const [infoPet, setInfoPet] = useState([]);
+  const [Loading, setLoading] = useState(false)
+
+  const GET_InfoPet = async (idPet) => {
+    console.log(`http://localhost:1337/api/pets/${idPet}?populate[picture][populate]=*`);
+    const response = await fetch(`http://localhost:1337/api/pets/${idPet}?populate[picture][populate]=*`);
+    const data = await response.json();
+
+
+    setInfoPet(data.data);
+    setLoading(true);
+  }
+
+  useEffect(() => {
+    GET_InfoPet(id);
+  }, [])
+
+
+
   return (
     <>
-    <section className='containerPet'>
-      <img src={CardTest} alt="" className='ImagenTest'/>
-        <article>
-            <h2>Dale un hogar a <span className='namePet'>PANCHO</span></h2>
-            <h4><i>Raza</i></h4>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non magnam reprehenderit velit sequi recusandae. Veritatis facilis dolorem sapiente laboriosam nisi illo esse optio minima recusandae illum itaque nesciunt, voluptates delectus?</p>
+      {!Loading ? 'CARGANDO' :  (
+        <section className='containerPet'>
+          <img src={`http://localhost:1337${infoPet.attributes.picture.data.attributes.url}`} alt="" className='ImagenTest' />
+          <article>
+            <h2>Dale un hogar a <span className='namePet'>{infoPet.attributes.name}</span></h2>
+            <h4><i className='descPet'>{infoPet.attributes.breed}</i></h4>
+            <p>{infoPet.attributes.description}</p>
             <div className="containerAtributos"></div>
             <button className='petBtn'>Adóptame</button>
-        </article>
-    </section>
+          </article>
+        </section>)
+      }
+
     </>
   )
 }
